79b33847c5d30b4dd2059c7f072277d86e54030a
na-Mama <nam0656@gmail.com>
2025-09-14 15:51:54 +0900

    fix: resolve Navigate import error and remove duplicate routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
